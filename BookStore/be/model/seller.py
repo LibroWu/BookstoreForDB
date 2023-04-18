d27affb9ee077060d3dcc6842d6bf60144116944
max-blabla <gooooooooooood@sjtu.edu.cn>
2023-04-18 17:36:42 +0800

    On branch myp_branch Changes to be committed: 	modified:   BookStore/be/model/error.py 	modified:   BookStore/be/model/seller.py

--- BookStore/be/model/seller.py
+++ BookStore/be/model/seller.py
@@ -1,4 +1,4 @@
-import sqlite3 as sqlite
+import pymongo
 from be.model import error
 from be.model import db_conn
 
@@ -16,11 +16,23 @@ class Seller(db_conn.DBConn):
                 return error.error_non_exist_store_id(store_id)
             if self.book_id_exist(store_id, book_id):
                 return error.error_exist_book_id(book_id)
-
-            self.conn.execute("INSERT into store(store_id, book_id, book_info, stock_level)"
-                              "VALUES (?, ?, ?, ?)", (store_id, book_id, book_json_str, stock_level))
-            self.conn.commit()
-        except sqlite.Error as e:
+            
+            
+            
+            bookshelfName = "bookshelf"
+            bookshelfCollection = self.db[bookshelfName]
+            bookshelfCollection.insert_one({"user_id":user_id,"store_id":store_id,"book_id":book_id,"stock_level":stock_level})
+            
+            bookInfoName = "bookInfo"
+            bookInfoName = self.db[bookInfoName]
+            bookInfoName.insert_one({"book_id":book_id,"book_info":book_json_str})
+            
+            
+            
+            # self.conn.execute("INSERT into store(store_id, book_id, book_info, stock_level)"
+            #                   "VALUES (?, ?, ?, ?)", (store_id, book_id, book_json_str, stock_level))
+            # self.conn.commit()
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
@@ -34,11 +46,15 @@ class Seller(db_conn.DBConn):
                 return error.error_non_exist_store_id(store_id)
             if not self.book_id_exist(store_id, book_id):
                 return error.error_non_exist_book_id(book_id)
-
-            self.conn.execute("UPDATE store SET stock_level = stock_level + ? "
-                              "WHERE store_id = ? AND book_id = ?", (add_stock_level, store_id, book_id))
-            self.conn.commit()
-        except sqlite.Error as e:
+            
+            bookshelfName = "bookshelf"
+            bookshelfCollection = self.db[bookshelfName]
+            query = {"user_id":user_id,"store_id":store_id,"book_id":book_id}
+            renew = {"$inc":{"stock_level":add_stock_level}}
+            bookshelfCollection.update(query,renew)
+        
+                        
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
@@ -50,10 +66,16 @@ class Seller(db_conn.DBConn):
                 return error.error_non_exist_user_id(user_id)
             if self.store_id_exist(store_id):
                 return error.error_exist_store_id(store_id)
-            self.conn.execute("INSERT into user_store(store_id, user_id)"
-                              "VALUES (?, ?)", (store_id, user_id))
-            self.conn.commit()
-        except sqlite.Error as e:
+            
+            
+            storeListName = "storeList" 
+            storeCollection = self.db[storeListName]
+            storeCollection.insert_one({"user_id":user_id,"store_id":store_id})
+             
+            # self.conn.execute("INSERT into user_store(store_id, user_id)"
+            #                   "VALUES (?, ?)", (store_id, user_id))
+            # self.conn.commit()
+        except pymongo.errors.PyMongoError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
