69f37e6615946a5ded7c1341c89f0134ce75f9c1
max-blabla <gooooooooooood@sjtu.edu.cn>
2023-04-21 20:04:09 +0800

    40 percent

--- BookStore/fe/access/buyer.py
+++ BookStore/fe/access/buyer.py
@@ -15,6 +15,51 @@ class Buyer:
         code, self.token = self.auth.login(self.user_id, self.password, self.terminal)
         assert code == 200
 
+    def confirm_receive(self,order_id:str):
+        json = {
+            "user_id": self.user_id,
+            "order_id":order_id,
+        }
+        url = urljoin(self.url_prefix,"confirm_send")
+        headers = {"token":self.token}
+        r = requests.post(url,headers=headers,json=json)
+        return r.status_code
+    
+    
+    def search_book(self,search_type:int,store_id:str,search_params:list):
+        json = {
+            "user_id": self.user_id,
+            "search_type":search_type,
+            "store_id": store_id,
+            "search_params":search_params
+        }
+        url = urljoin(self.url_prefix,"search_book")
+        headers = {"token":self.token}
+        r = requests.post(url,headers=headers,json=json)
+        response_json = r.json()
+        return r.status_code, response_json.get("book_name")
+    
+    def search_order(self):
+        json = {
+            "user_id": self.user_id,
+        }
+        url = urljoin(self.url_prefix,"search_order")
+        headers = {"token":self.token}
+        r = requests.post(url,headers=headers,json=json)
+        response_json = r.json()
+        return r.status_code, response_json.get("order_info")
+    
+    def cancel_order(self,order_id:str):
+        json = {
+            "user_id": self.user_id,
+            "order_id":order_id,
+        }
+        url = urljoin(self.url_prefix,"cancel_order")
+        headers = {"token":self.token}
+        r = requests.post(url,headers=headers,json=json)
+        return r.status_code
+
+
     def new_order(self, store_id: str, book_id_and_count: [(str, int)]) -> (int, str):
         books = []
         for id_count_pair in book_id_and_count:
